b1e1822c16fefdf0ac07966fe771faf182187381
Alican Keçici <alican.kecici@gmail.com>
2023-06-21 15:39:36 +0300

    secondMax_fun_added

--- src/main/kotlin/org/csystem/examples/homework_9/Application.kt
+++ src/main/kotlin/org/csystem/examples/homework_9/Application.kt
@@ -4,4 +4,18 @@ fun main()
 {
     val mm = MasterMindApp()
     mm.run()
+
+    val a = IntArray(10) {it + 1}
+    println(secondMax(a))
+}
+fun secondMax(arr : IntArray) : Int
+{
+    var max1 = arr[0]; var max2 = arr[0]
+
+    for (i in arr)
+        if (i > max1) {
+            max2 = max1
+            max1 = i
+    }
+    return max2
 }
